cb5fffff1c95437bc7d4c496af3a52332fc96a53
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-07-14 23:54:39 +0530

    Adds task editing functionality with rename capability
    Implements complete edit workflow allowing users to rename existing tasks through a dedicated edit page.
    
    Fixes variable naming bug in task display route that prevented proper data rendering.
    
    Updates UI layout to use responsive grid system and adds edit links to each task card for better user experience.

--- 06_project/index.js
+++ 06_project/index.js
@@ -16,12 +16,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tasks/:filename', (req, res) => {
-  fs.readFile(`./tasks/${req.params.filename}`, 'utf-8', (error, data) => res.render("show", {filename: req.params.filename, data: req.paramas.data}))
-})
+  fs.readFile(`./tasks/${req.params.filename}`, 'utf-8', (error, filedata) => res.render("show", {filename: req.params.filename, filedata: filedata}))
+});
+
+app.get('/edit/:filename', (req, res) => {
+  res.render('edit', {filename: req.params.filename});
+});
+
 
 app.post('/create', (req, res) => {
   fs.writeFile(`./tasks/${req.body.title.split(' ').join('-')}.txt`, req.body.details, (error) => console.log("Error: ", error));
   res.redirect("/");
+});
+
+app.post('/edit', (req, res) => {
+  fs.rename(`./tasks/${req.body.previous}`, `./tasks/${req.body.next.split(' ').join('-')}`, function(error) { res.redirect("/") });
 });
 
 app.listen(4000, () => {
